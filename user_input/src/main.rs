0aa0c68a11902ce91ca38244df1d8fce23ed2cdb
Szymon <szyszkaimyszka@gmail.com>
2026-01-16 11:57:06 +0100

    Add comments to understand what's going on

--- user_input/src/main.rs
+++ user_input/src/main.rs
@@ -1,8 +1,15 @@
+// Standard input output package
 use std::io;
 fn main() {
+    // print line "macro"
     println!("Hello, world!");
+    // declare mutable "input" variable which is a new string
     let mut input = String::new(); 
 
+    // input.standardinput read line
+    // mutate input variable
+    // (without &mut Rust just puts reference/copy of variable)
+    // if fail to read return message
     io::stdin().read_line(&mut input).expect("failed to read line");;
     println!("{}",input);
 }
