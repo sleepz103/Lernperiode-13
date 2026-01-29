36de90e6155a6dce3b308cd725787b1c7d6dbd99
Szymon <szyszkaimyszka@gmail.com>
2026-01-29 14:52:52 +0100

    Correct syntax errors and build string from game statistics

--- read_write/src/main.rs
+++ read_write/src/main.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::{self, Write, Read}; // Write has write_all()
+use std::io::{Write, Read}; // Write has write_all()
 
 
 fn main() -> std::io::Result<()> {
@@ -7,11 +7,15 @@ fn main() -> std::io::Result<()> {
     let mut file = File::open("stats.txt")?;
     let mut statistics = String::new();
     file.read_to_string(&mut statistics)?;
-    println!("File content:\n{}", statistics);
 
-    let win = statistics.chars().nth(0).unwrap();  // First character (win)
-    let lose = statistics.chars().nth(2).unwrap(); // Third character (lose, after comma)
+    // wrong: What if bigger that one char, for ex. 10?
+    let mut win: i32 = statistics.chars().nth(0).unwrap() as i32;  // First character (win)
+    let mut lose: i32 = statistics.chars().nth(2).unwrap() as i32; // Third character (lose, after comma)
 
+    // Displays correctly
+    println!("File content: {}", statistics);
+
+    // Error displays: 0,0 in file turns to 52 and 44
     println!("Wins: {}", win);
     println!("Losses: {}", lose);
 
@@ -19,10 +23,10 @@ fn main() -> std::io::Result<()> {
     win = win + 1;
     lose = lose + 1;
 
-    let new_stats = win.push_str(lose)
+    let new_stats: String = String::from(win.to_string() + "," + &lose.to_string());
 
-    let mut file = File::create("stats.txt")?;
-    file.write_all(stats.as_bytes())?;
+    let mut file: File = File::create("stats.txt")?;
+    file.write_all(new_stats.as_bytes())?;
     println!("Message written to file.");
 
     
